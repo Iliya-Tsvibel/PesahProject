8302cd7e53454b1cabd4b6eaa949cc38398cc2d1
Iliya-hacker <45923437+Iliya-hacker@users.noreply.github.com>
2019-04-28 01:48:47 +0300

    Add files via upload

---
+++ CalcProject/CalcProject/CalcProject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcProject
+{
+    class CalcProject
+    {
+        static SqlConnection con;
+        String sqlPath = @"Data Source=.;Initial Catalog=Calc Project;Integrated Security=true;";
+
+        public CalcProject()
+        {
+            con = new SqlConnection($"{sqlPath}");
+            con.Open();
+        }
+        public void AddNumbers(int x, int y)
+        {
+            using (SqlCommand cmd = new SqlCommand($"Insert Into XTable Values ({x}); Insert Into YTable Values ({y});", con))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void PrintTheCalculator()
+        {
+            using (SqlCommand cmd = new SqlCommand("C_Result", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default))
+                {
+                    while (reader.Read() == true)
+                    {
+                        Console.WriteLine($" {reader["X"]} {reader["Operation"]} {reader["Y"]} {reader["Result"]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CalcProject/CalcProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcProject calc = new CalcProject();
+         
+            
+                Console.Write("Insert the number:");
+                int x = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Insert another number:");
+                int y = Convert.ToInt16(Console.ReadLine());
+            if (x > 0 && y > 0)
+            {
+                calc.AddNumbers(x, y);
+                Console.WriteLine("Here All Options Of Your Calculator:");
+                calc.PrintTheCalculator();
+            }
+
+
+            else
+            {
+                
+                Console.WriteLine("The numbers must have positive values");
+            }
+
+           
+        }
+    }
+}
